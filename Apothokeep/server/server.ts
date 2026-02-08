eb5af8134c431f8164bad085e657d03e1d2dbf82
Andvrew <seyiababs@gmail.com>
2026-02-08 03:26:28 -0500

    sync

--- Apothokeep/server/server.ts
+++ Apothokeep/server/server.ts
@@ -4,13 +4,15 @@ import connectMongoDB from "./db/mongodb";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 (async () => {
   await connectMongoDB();
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+    console.log(`Local Access: http://localhost:${PORT}`);
+    console.log(`Network Access: http://172.21.80.128:${PORT}`);
   });
 })();
 
